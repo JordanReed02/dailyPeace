ae22be8ff0260106b69015d0926258886e0b0fdb
JordanReed02 <>
2021-04-11 10:34:51 -0400

    Working on working edit and delete buttons for orders

--- dailypeace/app/controllers/orders_controller.rb
+++ dailypeace/app/controllers/orders_controller.rb
@@ -20,27 +20,34 @@ end
 
 # show
 get "/orders/:id" do
-  @order = Order.find(params[:id])
+  @order = Order.find(params[:Order_id])
   @sage = Sage.find(params[:id])
   erb :show
 end
 
 # edit
 get "/orders/:id/edit" do
-  @order = Order.find(params[:orders_id])
+  @order = Order.find(params[:Order_id])
   erb :edit
 end
 
 # update
 patch "/orders/:id" do
   @order = Order.find(params[:orders_id])
-  @order.update(params[:order])
+if  @order.update(title: params[:orders_id])
+  flash[:success] = "The Sage understands"
   redirect  "/orders/#{ @order.id }"
+else
+  erb :edit
+  end
 end
-
 #destroy
-delete "/orders/:id" do
-  Order.destroy(params[:id])
+delete "/orders/delete" do
+  connection.execute("DELETE FROM orders WHERE orders.id = ?", params['id'])
+  if @order.nil?
+    flash[:error] = "Could not change Request"
+    @order.destroy
   redirect "/orders"
-end
-end
+      end
+    end
+  end
